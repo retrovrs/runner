e98e85f5d8f5d247b13c6e09c8927c5b621a97fb
Lucaslah <lucaslah@cssudii.tk>
2022-03-30 14:37:23 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,7 +1,85 @@
+ESC=$(printf '\033') RESET="${ESC}[0m" BLACK="${ESC}[30m" RED="${ESC}[31m"
+GREEN="${ESC}[32m" YELLOW="${ESC}[33m" BLUE="${ESC}[34m" MAGENTA="${ESC}[35m"
+CYAN="${ESC}[36m" WHITE="${ESC}[37m" DEFAULT="${ESC}[39m"
+
+greenprint() { printf "${GREEN}%s${RESET}\n" "$1"; }
+blueprint() { printf "${BLUE}%s${RESET}\n" "$1"; }
+redprint() { printf "${RED}%s${RESET}\n" "$1"; }
+yellowprint() { printf "${YELLOW}%s${RESET}\n" "$1"; }
+magentaprint() { printf "${MAGENTA}%s${RESET}\n" "$1"; }
+cyanprint() { printf "${CYAN}%s${RESET}\n" "$1"; }
+
 PS3="Select CPU architecture: "
 
-select arch in x86_64 x86
-do
-    echo "Selected architecture: $arch"
-    echo "Selected number: $REPLY"
+select arch in x86_64 x86 exit
+do  
+    case $arch in
+    x86_64)
+      # greenprint "Selected architecture: $arch"
+      ARCH=$arch
+      break
+      ;;
+    x86)
+      # greenprint "Selected architecture: $arch"
+      ARCH=$arch
+      break
+      ;;
+    exit)
+      redprint "exiting..."
+      break
+      ;;
+    *) 
+      redprint "Invalid option $REPLY"
+      ;;
+  esac
 done
+
+echo ""
+
+PS3="Select OS: "
+
+select os in linux osx windows exit
+do  
+    case $os in
+    linux)
+      # greenprint "Selected os: $os"
+      OS=$os
+      break
+      ;;
+    osx)
+      # greenprint "Selected os: $os"
+      OS=$os
+      break
+      ;;
+    windows)
+      # greenprint "Selected os: $os"
+      OS=$os
+      break
+      ;;
+    exit)
+      redprint "exiting..."
+      break
+      ;;
+    *) 
+      redprint "Invalid option $REPLY"
+      ;;
+  esac
+done
+
+echo ""
+
+read -p 'Enter number of runners: ' RUNNERS
+read -p 'Enter github token: ' GH_TOKEN
+
+greenprint "CPU Architecture: $ARCH"
+greenprint "Operting System: $OS"
+greenprint "Runners: $RUNNERS"
+greenprint "Token: $GH_TOKEN"
+
+echo ""
+
+greenprint "Starting..."
+
+sed -i '' "8s/.*/      - ACCESS_TOKEN=$GH_TOKEN/" docker-compose.yml
+
+greenprint "Done!"
